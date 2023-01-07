ac32f4bc9e1379f21e649b31e237409f063d4a91
Jose Ruas <joseluis.ruas@gmail.com>
2023-01-07 00:34:22 -0300

    feat: add http protocol

--- src/presentation/controllers/create-order/create-charge.ts
+++ src/presentation/controllers/create-order/create-charge.ts
@@ -1,7 +1,8 @@
 import { MissingParamError } from '../../error'
+import { HttpRequest, HttpResponse } from '../../protocols'
 
 export class CreateChargeController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     return {
       statusCode: 400,
       body: new MissingParamError('charges')
