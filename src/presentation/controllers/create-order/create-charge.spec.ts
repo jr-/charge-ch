5125c52103037393e7c5e110af4e183c21618a1f
Jose Ruas <joseluis.ruas@gmail.com>
2023-01-07 00:29:21 -0300

    refactor: move sut creation to a makeSut function

--- src/presentation/controllers/create-order/create-charge.spec.ts
+++ src/presentation/controllers/create-order/create-charge.spec.ts
@@ -1,9 +1,13 @@
 import { CreateChargeController } from './create-charge'
 import { MissingParamError } from '../error'
 
+const makeSut = (): CreateChargeController => {
+  return new CreateChargeController()
+}
+
 describe('Create Charge Controller', () => {
   test('Should return 400 if no charges are provided', () => {
-    const sut = new CreateChargeController()
+    const sut = makeSut()
     const httpRequest = {
       body: {}
     }
