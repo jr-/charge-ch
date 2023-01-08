73c3ded79ecca1c8d631a68af9c1182dd94979f3
Jose Ruas <joseluis.ruas@gmail.com>
2023-01-08 12:42:30 -0300

    test: ensure create charge has only valid cpfs

--- src/presentation/controllers/create-charge/create-charge.spec.ts
+++ src/presentation/controllers/create-charge/create-charge.spec.ts
@@ -1,6 +1,7 @@
 import { CreateChargeController } from './create-charge'
 import { InvalidParamError, MissingParamError, ServerError } from '../../error'
 import { EmailValidator } from '../../protocols/email-validator'
+import { CpfValidator } from '../../protocols/cpf-validator'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -11,18 +12,30 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
+const makeCpfValidator = (): CpfValidator => {
+  class CpfValidatorStub implements CpfValidator {
+    isValid (cpf: string): boolean {
+      return true
+    }
+  }
+  return new CpfValidatorStub()
+}
+
 interface SutTypes {
   sut: CreateChargeController
   emailValidatorStub: EmailValidator
+  cpfValidatorStub: CpfValidator
 }
 
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
-  const sut = new CreateChargeController(emailValidatorStub)
+  const cpfValidatorStub = makeCpfValidator()
+  const sut = new CreateChargeController(emailValidatorStub, cpfValidatorStub)
 
   return {
     sut,
-    emailValidatorStub
+    emailValidatorStub,
+    cpfValidatorStub
   }
 }
 
@@ -215,6 +228,73 @@ describe('Create Charge Controller', () => {
   test('Should return 500 if EmailValidator throws', async () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            governamentId: 'any_governament_id',
+            debtAmount: 'any_debt_amount',
+            debtDueDate: 'any_date',
+            debtId: 'any_id'
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 400 if an invalid cpf is provided', async () => {
+    const { sut, cpfValidatorStub } = makeSut()
+    jest.spyOn(cpfValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            governamentId: 'invalid_governament_id',
+            debtAmount: 'any_debt_amount',
+            debtDueDate: 'any_date',
+            debtId: 'any_id'
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('governamentId'))
+  })
+
+  test('Should call CpfValidator with correct cpf', async () => {
+    const { sut, cpfValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(cpfValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            governamentId: 'any_governament_id',
+            debtAmount: 'any_debt_amount',
+            debtDueDate: 'any_date',
+            debtId: 'any_id'
+          }
+        ]
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_governament_id')
+  })
+
+  test('Should return 500 if CpfValidator throws', async () => {
+    const { sut, cpfValidatorStub } = makeSut()
+    jest.spyOn(cpfValidatorStub, 'isValid').mockImplementationOnce(() => {
       throw new Error()
     })
     const httpRequest = {
