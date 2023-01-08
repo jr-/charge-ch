0a6a2a8eb7ee5a024ed3152ae479354c418102d9
Jose Ruas <joseluis.ruas@gmail.com>
2023-01-08 11:18:47 -0300

    feat: ensure no charge json missing a required param

--- src/presentation/controllers/create-charge/create-charge.ts
+++ src/presentation/controllers/create-charge/create-charge.ts
@@ -6,16 +6,22 @@ export class CreateChargeController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { body } = httpRequest
 
-    const requiredFields = ['charges']
-    for (const field of requiredFields) {
-      if (!body[field]) {
-        return badRequest(new MissingParamError(field))
-      }
+    if (!body.charges) {
+      return badRequest(new MissingParamError('charges'))
     }
 
     const { charges } = body
     if (charges && Object.keys(charges).length === 0) {
       return badRequest(new InvalidParamError('charges'))
+    }
+
+    const requiredChargeFields = ['name', 'governamentId', 'email', 'debtAmount', 'debtDueDate', 'debtId']
+    for (const charge of body.charges) {
+      for (const field of requiredChargeFields) {
+        if (!charge[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
     }
 
     return ok({})
