d769fb1a61aaac4041cda9fb1133b4f175fcec17
Jose Ruas <joseluis.ruas@gmail.com>
2023-01-08 11:19:09 -0300

    test: ensure no charge json missing a required param

--- src/presentation/controllers/create-charge/create-charge.spec.ts
+++ src/presentation/controllers/create-charge/create-charge.spec.ts
@@ -20,11 +20,131 @@ describe('Create Charge Controller', () => {
     const sut = makeSut()
     const httpRequest = {
       body: {
-        charges: {}
+        charges: []
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('charges'))
+  })
+
+  test('Should return 400 if no email is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            name: 'any_name',
+            governamentId: 'any_governament_id',
+            debtAmount: 'any_debt_amount',
+            debtDueDate: 'any_date',
+            debtId: 'any_id'
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if no name is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            email: 'any_email@mail.com',
+            governamentId: 'any_governament_id',
+            debtAmount: 'any_debt_amount',
+            debtDueDate: 'any_date',
+            debtId: 'any_id'
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+  })
+
+  test('Should return 400 if no governamentId is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            debtAmount: 'any_debt_amount',
+            debtDueDate: 'any_date',
+            debtId: 'any_id'
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('governamentId'))
+  })
+
+  test('Should return 400 if no debtAmount is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            governamentId: 'any_governament_id',
+            debtDueDate: 'any_date',
+            debtId: 'any_id'
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('debtAmount'))
+  })
+
+  test('Should return 400 if no debtDueDate is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            governamentId: 'any_governament_id',
+            debtAmount: 'any_debt_amount',
+            debtId: 'any_id'
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('debtDueDate'))
+  })
+
+  test('Should return 400 if no debtId is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        charges: [
+          {
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            governamentId: 'any_governament_id',
+            debtAmount: 'any_debt_amount',
+            debtDueDate: 'any_date'
+          }
+        ]
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('debtId'))
   })
 })
