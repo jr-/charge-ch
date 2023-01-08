d3dbc9e12897722dc7c0d35fb66a78cd5176620b
Jose Ruas <joseluis.ruas@gmail.com>
2023-01-08 20:21:21 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/charge-routes.test.ts
+++ src/main/routes/charge-routes.test.ts
@@ -5,7 +5,18 @@ describe('Charge Routes', () => {
   test('Should return ok on success', async () => {
     await request(app)
       .post('/api/charge/bulk')
-      .send({})
+      .send({
+        charges: [
+          {
+            name: 'agua',
+            governamentId: '15906606092',
+            email: 'joseluis.ruas@gmail.com',
+            debtAmount: '1000.00',
+            debtDueDate: '2023-01-20',
+            debtId: '8291'
+          }
+        ]
+      })
       .expect(200)
   })
 })
--- src/main/routes/charge-routes.ts
+++ src/main/routes/charge-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { makeCreateChargeBulkController } from '../factories/charge'
+import { adaptRoute } from '../adapters/express-route-adapter'
 
 export default (router: Router): void => {
-  router.post('/charge/bulk', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  router.post('/charge/bulk', adaptRoute(makeCreateChargeBulkController()))
 }
