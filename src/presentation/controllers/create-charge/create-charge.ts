5c553e32a851e50388ed0a6e8229c8a5d385cf6d
Jose Ruas <joseluis.ruas@gmail.com>
2023-01-08 12:28:46 -0300

    feat: ensure CreateChargeController return 500 if EmailValidator throws

--- src/presentation/controllers/create-charge/create-charge.ts
+++ src/presentation/controllers/create-charge/create-charge.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError, MissingParamError } from '../../error'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest, ok } from '../../helper/http-helper'
+import { badRequest, ok, serverError } from '../../helper/http-helper'
 import { EmailValidator } from '../../protocols/email-validator'
 
 export class CreateChargeController implements Controller {
@@ -11,33 +11,37 @@ export class CreateChargeController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { body } = httpRequest
+    try {
+      const { body } = httpRequest
 
-    if (!body.charges) {
-      return badRequest(new MissingParamError('charges'))
-    }
+      if (!body.charges) {
+        return badRequest(new MissingParamError('charges'))
+      }
 
-    const { charges } = body
-    if (charges && Object.keys(charges).length === 0) {
-      return badRequest(new InvalidParamError('charges'))
-    }
+      const { charges } = body
+      if (charges && Object.keys(charges).length === 0) {
+        return badRequest(new InvalidParamError('charges'))
+      }
 
-    const requiredChargeFields = ['name', 'governamentId', 'email', 'debtAmount', 'debtDueDate', 'debtId']
-    for (const charge of body.charges) {
-      for (const field of requiredChargeFields) {
-        if (!charge[field]) {
-          return badRequest(new MissingParamError(field))
+      const requiredChargeFields = ['name', 'governamentId', 'email', 'debtAmount', 'debtDueDate', 'debtId']
+      for (const charge of body.charges) {
+        for (const field of requiredChargeFields) {
+          if (!charge[field]) {
+            return badRequest(new MissingParamError(field))
+          }
         }
       }
-    }
 
-    for (const charge of body.charges) {
-      const isValid = this.emailValidator.isValid(charge.email)
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
+      for (const charge of body.charges) {
+        const isValid = this.emailValidator.isValid(charge.email)
+        if (!isValid) {
+          return badRequest(new InvalidParamError('email'))
+        }
       }
-    }
 
-    return ok({})
+      return ok({})
+    } catch (error) {
+      return serverError()
+    }
   }
 }
