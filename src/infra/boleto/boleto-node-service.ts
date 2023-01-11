7efafe9eba8c4dacb9c09b02cb14335905033efc
Jose Ruas <joseluis.ruas@gmail.com>
2023-01-11 14:38:26 -0300

    refactor: remove constants to charge business rules, like charger infos

--- src/infra/boleto/boleto-node-service.ts
+++ src/infra/boleto/boleto-node-service.ts
@@ -1,6 +1,7 @@
 import { AddChargeModel } from '../../domain/usecases/create-charges'
 import parseISO from 'date-fns/parseISO'
 import { GenerateBoleto } from '../../data/protocols/generate-boleto'
+import env from '../../main/config/env'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Boleto = require('boleto-node').Boleto
@@ -10,28 +11,19 @@ export class BoletoNodeService implements GenerateBoleto {
     const debtAmount = chargeInfo.debtAmount.replace(/\./g, '')
 
     const boleto = new Boleto({
-      banco: 'bradesco',
+      banco: env.boletoCharge.chargerInfo.bankName,
       data_emissao: new Date(),
       data_vencimento: parseISO(chargeInfo.debtDueDate),
       valor: debtAmount,
       nosso_numero: '1234567',
       numero_documento: '123123',
-      instrucoes: 'Não receber após vencimento. \n Multa de 2% após o vencimento. Juros de 0,03% de mora ao dia.', // separar cada linha por \n
-      pagador: 'TESTE DA SILVA',
-      pagador_cpf_cnpj: '288.664.364-53',
-      pagador_endereco_rua_num: 'RUA SÃO CARLOS AUGUSTINO DA SILVA, 50',
-      pagador_endereco_bairro: 'SÃO JOÃO BATISTA DA SILVA',
-      pagador_endereco_cep: '15.160-100',
-      pagador_endereco_cidade_estado: 'SÃO JOSÉ DOS TESTES - SP',
-      pagador_outras_informacoes: 'Login da central: testeteste',
-      cedente: 'PAGAMENTOS LTDA',
-      cedente_cnpj: '47322759000154',
-      cedente_endereco_rua_num: 'RUA CEL. JONAS DOS SANTOS, 130',
-      cedente_endereco_bairro: 'CENTRO',
-      cedente_endereco_cep: '15.115-100',
-      cedente_endereco_cidade_estado: 'SÃO JOSÉ DOS TESTES - SP',
-      agencia: '6119',
-      codigo_cedente: '001.584-2',
+      instrucoes: env.boletoCharge.chargerInfo.instructions,
+      pagador: chargeInfo.name,
+      pagador_cpf_cnpj: chargeInfo.governamentId,
+      cedente: env.boletoCharge.chargerInfo.name,
+      cedente_cnpj: env.boletoCharge.chargerInfo.cnpj,
+      agencia: env.boletoCharge.chargerInfo.bankAgency,
+      codigo_cedente: env.boletoCharge.chargerInfo.bankCode,
       carteira: '09'
     })
 
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -9,5 +9,15 @@ export default {
     from: process.env.EMAIL_CHARGE_FROM || '"Fred Foo 👻" <foo@example.com>',
     subject: process.env.EMAIL_CHARGE_SUBJECT || 'Pague sua cobranca',
     text: process.env.EMAIL_CHARGE_TEXT || 'Segue o boleto abaixo para pagamento'
+  },
+  boletoCharge: {
+    chargerInfo: {
+      name: process.env.BOLETO_CHARGE_CHARGER_NAME || 'Kanastra',
+      cnpj: process.env.BOLETO_CHARGE_CHARGER_CNPJ || '47322759000154',
+      bankCode: process.env.BOLETO_CHARGE_CHARGER_BANK_CODE || '001.584-2',
+      bankAgency: process.env.BOLETO_CHARGE_CHARGER_BANK_AGENCY || '6119',
+      bankName: process.env.BOLETO_CHARGE_CHARGER_BANK_NAME || 'bradesco',
+      instructions: process.env.BOLETO_CHARGE_CHARGER_INSTRUCTIONS || 'Não receber após vencimento. \n Multa de 2% após o vencimento. Juros de 0,03% de mora ao dia.'
+    }
   }
 }
